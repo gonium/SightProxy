f5b002d785777fca538871e400839dbe59c935a6
Jamorham <jamorham@users.noreply.github.com>
2017-07-05 19:12:43 +0100

    Pump emulator fix

--- lib/pump_emulator.py
+++ lib/pump_emulator.py
@@ -1,7 +1,8 @@
 from keystore import *
 from packet_factory import *
+from app_factory import *
 from cryptograph import *
-
+from dirtyemulator import *
 # from test_packets import *
 
 # pump emulator
@@ -13,10 +14,19 @@ SECRET_KEY = None
 PUMP_EMU_INCOMING_KEY = None
 PUMP_EMU_OUTGOING_KEY = None
 
+PUMP_APP_EMULATION = False
+
+LAST_CHALLENGE = None
+
+T_FILE = "/tmp/cx.trigger"
+R_FOLDER = "/tmp/cx-reply-"
+
+def init_pump_emulator():
+    global PUMP_APP_EMULATION
+    PUMP_APP_EMULATION = boolean_get('setting-PUMP_APP_EMULATION')
 
 def generate_pump_response(data, logger=None, VERBOSE_LOGS=True):
-    # TODO replace with better storage
-    global HIGHEST_NONCE
+    global HIGHEST_NONCE, LAST_CHALLENGE
     global PUMP_RANDOM_DATA, PEER_RANDOM_DATA, PEER_RANDOM_DATA, SECRET_KEY, PUMP_EMU_INCOMING_KEY, PUMP_EMU_OUTGOING_KEY
 
     if (data == None):
@@ -39,14 +49,24 @@ def generate_pump_response(data, logger=None, VERBOSE_LOGS=True):
             logger.critical("Trailer CCM INVALID! - skipping")
             return None
 
+        if (r['command'] == 'SynRequest'):
+            print "Replying with SynAckResponse"
+            # TODO CHECK COMID
+
+            PUMP_EMU_INCOMING_KEY = key_get('real_client_incoming')
+            PUMP_EMU_OUTGOING_KEY = key_get('real_client_outgoing')
+
+            if (PUMP_EMU_OUTGOING_KEY == None):
+                PUMP_EMU_OUTGOING_KEY = '\x00' * 16
+
+            reply = build_SynAckResponse(comid=r['records']['ComID'], nonce=r['records']['Nonce'],
+                                         channel='in',
+                                         key=PUMP_EMU_OUTGOING_KEY)
+
         if (r['command'] == 'ConnectionRequest'):
             HIGHEST_NONCE['in'] = 0
             print "Replying with ConnectionResponse"
             reply = build_ConnectionResponse(comid=r['records']['ComID'])
-
-        if (r['command'] == 'SynRequest'):
-            print "Replying with SynAckResponse"
-            reply = build_SynAckResponse(comid=r['records']['ComID'], nonce=r['records']['Nonce'], channel='in')
 
         if (r['command'] == 'KeyRequest'):
             print "Replying with KeyResponse"
@@ -77,17 +97,75 @@ def generate_pump_response(data, logger=None, VERBOSE_LOGS=True):
             reply = build_VerifyConfirmResponse(comid=r['records']['ComID'], nonce=r['records']['Nonce'],
                                                 key=PUMP_EMU_OUTGOING_KEY,
                                                 channel='in')
+            key_set("in-comid", r['records']['ComID'])
+
+        if PUMP_APP_EMULATION:
+            if (r['command'] == 'Data'):
+                print "Attempting to process data packet"
+                dirtyreply = getDirtyPumpReply(r['records']['Decrypted'])
+                if (dirtyreply):
+                    reply = build_DataPacket(comid=r['records']['ComID'], nonce=r['records']['Nonce'],
+                                             key=PUMP_EMU_OUTGOING_KEY,
+                                             channel='in',
+                                             data=dirtyreply.replace(" ", "").decode('hex'))
+                else:
+
+                    app_parsed = parse_app_packet(r['records']['Decrypted'])
+                    if (app_parsed['status'] == 'identified'):
+                        pretty(app_parsed['records'], VERBOSE_PRETTY=True)
+
+                    (service, command) = getAppLayerComponents(r['records']['Decrypted'])
+                    if (service == 0x00 and command == 0xD2F3):
+
+                        challenge = None
+                        if (os.path.exists(T_FILE)):
+                            f = open(T_FILE, "rb")
+                            tr = f.read()
+                            f.close()
+                            try:
+                                challenge = tr.decode('hex')
+                            except:
+                                print "Exception decoding"
+                                challenge = None
+                            if len(challenge) != 16:
+                                print "invalid length"
+                                challenge = None
+                        if (challenge != None):
+                            LAST_CHALLENGE = challenge
+                            logger.critical("SENDING CHALLENGE REQUEST: " + hd(
+                                r['records']['Decrypted']) + " " + hd(
+                                challenge))
+
+                        reply = build_AppServiceChallenge(comid=r['records']['ComID'], nonce=r['records']['Nonce'],
+                                                          key=PUMP_EMU_OUTGOING_KEY,
+                                                          channel='in', challenge=challenge,
+                                                          )
+
+                    if (service == 0x00 and command == 0xF7F0):
+                        logger.critical(
+                            "RECEIVED CHALLENGE RESPONSE: " + hex(app_parsed['records']['ChallengeService']) + " " + hd(
+                                app_parsed['records']['Challenge']))
+                        print "Generating challenge accepted"
+                        if (LAST_CHALLENGE != None):
+                            f = open(R_FOLDER + hd(LAST_CHALLENGE).upper(), "wb")
+                            f.write(hd(app_parsed['records']['Challenge']))
+                            f.close()
+                        print "building service activate confirmed"
+                        reply = build_ServiceActivateConfirmed(comid=r['records']['ComID'], nonce=r['records']['Nonce'],
+                                                               key=PUMP_EMU_OUTGOING_KEY,
+                                                               channel='in',
+                                                               activated_service=app_parsed['records'][
+                                                                   'ChallengeService']
+                                                               )
+                    else:
+                        print "Don't know how to reply"
+
     else:
         print r
 
     x = parse_packet(reply, key=PUMP_EMU_OUTGOING_KEY)
     log_string = "\nPUMP EMULATOR REPLY\n" + x['status'] + "\n" + pretty_parsed_string(x) + "\n"
 
-    # print x['status']
-    # pretty_parsed(x)
-    # if (x['status'] == "identified"):
-    #     pretty(x['records'])
-    # print
 
     if (VERBOSE_LOGS == True):
         logger.info("\n" + log_string)
